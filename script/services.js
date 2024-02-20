ec3f65e96963f3ebaafb9df317801330fb3542d7
Demakova Tatiana <demakova_ts@mail.ru>
2024-02-20 14:35:46 +0700

    update css

---
+++ script/services.js
@@ -0,0 +1,27 @@
+let services = [
+    {
+        Name: "санузла",
+        photo: "img/services/bathroom_services.png",
+        price: 100000,
+    },
+    {
+        Name: "cпальни",
+        photo: "img/services/bedroom_services.png",
+        price: 120000,
+    },
+    {
+        Name: "кухни",
+        photo: "img/services/kitchen_services.png",
+        price: 150000,
+    },
+];
+
+// let divContacts = document.querySelector(".team__flex");
+
+// for (let employee of employees) {
+//     divContacts.insertAdjacentHTML(
+//         "beforeend",
+//         `<figure class="team__box"><img class="team__img" src="${employee.photo}" alt="Photo">
+//       <figcaption class="team__name">${employee.Name}</figcaption></figure>`
+//     );
+// }
