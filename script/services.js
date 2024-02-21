1d66d3552ea2e71a8a655ab487c1a04b5eddd5f3
Demakova Tatiana <demakova_ts@mail.ru>
2024-02-22 01:29:56 +0700

    test services.js

--- script/services.js
+++ script/services.js
@@ -3,25 +3,24 @@ let services = [
         Name: "санузла",
         photo: "img/services/bathroom_services.png",
         price: 100000,
+        info: ["Анализ. Важно оценить размер помещений, конструктивные особенности, высоту потолков, местоположение дверных и&nbsp;оконных проемов.", "Планирование. На&nbsp;этом этапе необходимо рассчитать количество материалов, продумать освещение, подобрать цвета и&nbsp;наметить конструктивные элементы (ниши и&nbsp;перегородки)."],
     },
     {
         Name: "cпальни",
         photo: "img/services/bedroom_services.png",
         price: 120000,
+        info: ["Анализ. Важно оценить размер помещений, конструктивные особенности, высоту потолков, местоположение дверных и&nbsp;оконных проемов.", "Планирование. На&nbsp;этом этапе необходимо рассчитать количество материалов, продумать освещение, подобрать цвета и&nbsp;наметить конструктивные элементы (ниши и&nbsp;перегородки)."],
     },
     {
         Name: "кухни",
         photo: "img/services/kitchen_services.png",
         price: 150000,
+        info: ["Анализ. Важно оценить размер помещений, конструктивные особенности, высоту потолков, местоположение дверных и&nbsp;оконных проемов.", "Планирование. На&nbsp;этом этапе необходимо рассчитать количество материалов, продумать освещение, подобрать цвета и&nbsp;наметить конструктивные элементы (ниши и&nbsp;перегородки)."],
     },
 ];
 
-// let divContacts = document.querySelector(".team__flex");
+let divContacts = document.querySelector(".catalog__conteiner");
 
-// for (let employee of employees) {
-//     divContacts.insertAdjacentHTML(
-//         "beforeend",
-//         `<figure class="team__box"><img class="team__img" src="${employee.photo}" alt="Photo">
-//       <figcaption class="team__name">${employee.Name}</figcaption></figure>`
-//     );
-// }
+for (let service of services) {
+
+}
