229d83d9bd37386691c069d166a19a29aa57cdc5
Demakova Tatiana <demakova_ts@mail.ru>
2024-02-27 18:15:21 +0700

    update services_page.js, changing the insertion of elements on the page

--- script/services_page.js
+++ script/services_page.js
@@ -1,37 +1,37 @@
-import {services as catalog} from './list_services.js';
+import { services as catalog } from './list_services.js';
 
 let divServ = document.querySelector(".catalog__conteiner");
 
-for (let i=0; i<catalog.length; i++) {
-    if (i%2 == 0) {
-    divServ.insertAdjacentHTML(
-        "beforeend",
-        `<div class="catalog__box">
-        <article class="catalog__desc desc${i}">
-        <img class="catalog__photo" src="${catalog[i].photo}" alt="interior design project photography">
-        <div class="catalog__box-info">
-        <h3 class="catalog__info">Дизайн-проект <br> интерьера ${catalog[i].Name}</h3>
-        <p class="catalog__info catalog__info_price">От ${catalog[i].price} ₽ </p>
-        </div></article></div>`)
+for (let i = 0; i < catalog.length; i++) {
+    if (i % 2 == 0) {
+        divServ.insertAdjacentHTML(
+            "beforeend",
+            `<div class="catalog__box">
+            <article id ="desc${i}" class="catalog__desc"></article></div>`)
     }
     else {
-    divServ.insertAdjacentHTML(
-        "beforeend",
-        `<div class="catalog__box catalog__box_position-reverse">
-        <article class="catalog__desc desc${i}">
-        <img class="catalog__photo" src="${catalog[i].photo}" alt="interior design project photography">
+        divServ.insertAdjacentHTML(
+            "beforeend",
+            `<div class="catalog__box catalog__box_position-reverse">
+            <article id ="desc${i}" class="catalog__desc"></article></div>`)
+    }//вставка внутрь elem, в конец.
+
+    const desc = document.getElementById(`desc${i}`);
+
+    desc.insertAdjacentHTML('afterBegin',
+        `<img class="catalog__photo" src="${catalog[i].photo}" alt="interior design project photography">
         <div class="catalog__box-info">
         <h3 class="catalog__info">Дизайн-проект <br> интерьера ${catalog[i].Name}</h3>
         <p class="catalog__info catalog__info_price">От ${catalog[i].price} ₽
-        </p></div></article></div>`)
-    }
+        </p></div>`
+    ) //вставка во внуть элемента, в начало
 
-    const features = document.querySelector(`.desc${i}`);
-    features.insertAdjacentHTML('afterEnd', `<div class="catalog__features"><ul class="ul${i}"></ul></div>`)
-    let ul = document.querySelector(`.ul${i}`);
-    for(let p of catalog[i].info) {
+    desc.insertAdjacentHTML('afterEnd', `<div id ="ul${i}" class="catalog__features"><ul class="catalog__ul"></ul></div>`) //после elem.
+
+    let ul = document.getElementById(`ul${i}`);
+    for (let p of catalog[i].info) {
         let item = document.createElement('li');
-        item.className ="catalog__text"
+        item.className = "catalog__text"
         item.innerHTML = p;
         ul.append(item);
     }
