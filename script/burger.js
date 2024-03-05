76e19972745637b8e76ceb5747f831948e6aea52
Demakova Tatiana <demakova_ts@mail.ru>
2024-03-05 15:04:50 +0700

    burger menu using details

---
+++ script/burger.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'flex' : 'none';
+ });
